f15a7deee6ce870240250595186d07e013d5d095
Linxerz <pruittj2000@Yahoo.com>
2020-03-18 15:53:32 -0500

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,16 @@
-def hello_t
-
+def hello_t(array)
+    i = 0
+    if block_given?
+        while i < array.length
+        yield(array[i])
+        i += 1
+        end
+        array
+    else
+    puts "Hey! No block was given!"
+    end
 end
 
 # call your method here!
 
+hello_t(["Tim","Tom","Jim"]){|name| puts name}
